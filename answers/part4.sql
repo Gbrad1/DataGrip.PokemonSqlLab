284ff0a5d16d93f219dbcd63927390602b2e97ff
Giles Bradford <gbradford@Gbrads-Mac.local>
2020-03-11 15:19:07 -0400

    saving

--- answers/part4.sql
+++ answers/part4.sql
@@ -12,4 +12,8 @@ LEFT JOIN pokemons AS poke ON poke.id = pt.pokemon_id
 LEFT JOIN types AS typeP ON poke.primary_type = typeP.id
 LEFT JOIN types AS typeS ON poke.secondary_type = typeS.id
 GROUP BY poke.name, pokeBall.trainername, pokelevel, defense, maxhp, spDef, typeP.name, typeS.name
-ORDER BY AVG(defense + maxHP + spDef);
+ORDER BY AVG(defense + maxHP + spDef) DESC;
+
+I decided to wager a battle against Chris. He went for attack + spAtk + spdAtk while I went for
+defense + maxHP + spDef. In this query, Sim Trainer Geplin and his absolutely ridiculous defense Level
+comes out on top.
